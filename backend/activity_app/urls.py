c7f1f9f3e68d7de11c93acc7923ac03c76809d79
YOUR_NAME <YOUR_EMAIL>
2025-04-28 14:09:43 -0500

    Added endpoints paths to project.urls.

---
+++ backend/activity_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# from .views
+
+# urlpatterns = [
+#   path('all/<int:trip_id>', Activities.as_view(), name="activites")
+#   path('<int:id>/', Activity.as_view(), name="activity")
+# ]
--- backend/city_scope_project/urls.py
+++ backend/city_scope_project/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('user_app.urls')), # directs Django to the user_app.urls file
+    path('api/v1/activity/', include('activity_app.urls')), # directs django to the activity_app.urls
+    path('api/v1/stay/', include('stay_app.urls')), # directs django to the stay_app.urls
+    path('api/v1/itinerary/', include('itinerary_app.urls')), # directs django to the itinerary_app.urls
 ]

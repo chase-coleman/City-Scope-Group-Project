a97c9579afd3acf0cfd5a185d146437a485f3eb4
YOUR_NAME <YOUR_EMAIL>
2025-04-28 14:58:25 -0500

    created view to see all activities belonging to an itinerary

--- backend/activity_app/serializers.py
+++ backend/activity_app/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework import serializers
 from .models import Activity
 
-class ActivitySerializer(ModelSerializer):
+class Activity_Serializer(ModelSerializer):
   class Meta: 
     model = Activity
     fields = ['id', 'name', 'location', 'address', 'url']

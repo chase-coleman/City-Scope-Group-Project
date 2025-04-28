1917db26917890bc6be82c4ce0e58b6c11873013
poptaro <noahahn199@gmail.com>
2025-04-28 16:20:16 -0500

    adjusting models to have itinerary show all information regarding acitivites and stays. stays related name for itinerary also changed

--- backend/itinerary_app/serializers.py
+++ backend/itinerary_app/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 from .models import Itinerary
 
+from stay_app.serializers import Stay_Serializer
+# from activity_app.serializers import Activity_Serializer
+
 class Itinerary_Serializer(serializers.ModelSerializer):
+  stays = Stay_Serializer(many=True)
+  # activities = Activity_Serializer(many=True)
 
   class Meta:
     model = Itinerary
--- backend/itinerary_app/views.py
+++ backend/itinerary_app/views.py
@@ -18,12 +18,19 @@ class Itinerary_View(APIView):
     try:
       user_itinerary = get_object_or_404(Itinerary, id=id)
       user_itinerary_ser = Itinerary_Serializer(user_itinerary).data
-      return Response(user_itinerary_ser)
+      return Response(user_itinerary_ser, status=s.HTTP_200_OK)
     except:
-      return Response(f"Failed to fetch itinerary of ID: {id}")
+      return Response(f"Failed to fetch itinerary of ID: {id}", status=s.HTTP_404_NOT_FOUND)
 
   def post(self, request, id=None):
-    pass
+    try:
+      # The date the user inputs from the frontend in format YYYY-MM-DD
+      date = request.data.date
+      Itinerary.objects.create(date=date)
+      return Response(f"Itinerary created for the date: {date}")
+    except:
+      return Response(f"Failed to create an itinerary for the date: {date}")
+
 
 
 # Get all Itineries pertaining to a a trip
@@ -31,6 +38,6 @@ class Itinerary_View_All(APIView):
   def get(self, request, trip_id):
     try:
       users_itineraries = Itinerary.objects.filter(trip = trip_id).all()
-      
+
     except:
       return Response(f"Failed to retrieve all Itineraries pertaining to trip ID: {trip_id}")

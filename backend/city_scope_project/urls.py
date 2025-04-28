067647b28371bade00b106bf50031f985fcba99a
YOUR_NAME <YOUR_EMAIL>
2025-04-28 15:10:14 -0500

    added path to stay_app

--- backend/city_scope_project/urls.py
+++ backend/city_scope_project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('user_app.urls')), # directs Django to the user_app.urls file
+    path('api/v1/stay/', include('stay_app.urls')),
     # path('api/v1/activity/', include('activity_app.urls')), # directs django to the activity_app.urls
 ]

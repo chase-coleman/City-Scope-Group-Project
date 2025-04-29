fece1ccd4ee6f2eb554110cc091a8ffd3c2e593e
timskillbridge <timskillbridge@gmail.com>
2025-04-29 11:17:46 -0500

    last pull added garbage to my migrations files . . . fixed

--- backend/city_scope_project/urls.py
+++ backend/city_scope_project/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('api/v1/user/', include('user_app.urls')), # directs Django to the user_app.urls file
     path('api/v1/itinerary/', include('itinerary_app.urls')),
     path('api/v1/stay/', include('stay_app.urls')),
-<<<<<<< HEAD
-=======
     path('api/v1/trip/', include('trip_app.urls')),
->>>>>>> e4638c44c47ef1de2d304ac02ec1c207916853f4
     path('api/v1/activity/', include('activity_app.urls')), # directs django to the activity_app.urls
 ]

1ed1e7ee5d1eb0a97cac160b3870fc02e36d88d3
YOUR_NAME <YOUR_EMAIL>
2025-04-28 21:07:13 -0500

    Created PUT request capability to an individual activity.

--- backend/activity_app/views.py
+++ backend/activity_app/views.py
@@ -8,6 +8,7 @@ from itinerary_app.models import Itinerary
 
 class Activities(TokenReq):
   def get(self, request, itinerary_id):
+    # get all activities belonging to the itinerary matching itinerary_id
     itinerary_activities = Activity.objects.filter(itineraries=itinerary_id)
     serialized_activities = Activity_Serializer(itinerary_activities, many=True)
     return Response(serialized_activities.data, status=s.HTTP_200_OK)
@@ -23,10 +24,26 @@ class Activities(TokenReq):
     if serialized_activity.is_valid():
       serialized_activity.save() # save it to the database
       return Response(serialized_activity.data, status=s.HTTP_201_CREATED)
+    # if not, return errors
     return Response(serialized_activity.errors, status=s.HTTP_400_BAD_REQUEST)
   
 class Single_Activity(TokenReq):
   def get(self, request, id):
+    # grab the activity instance
     activity = get_object_or_404(Activity, id=id)
+    # serialize it
     serialized_activity = Activity_Serializer(activity)
     return Response(serialized_activity.data, status=s.HTTP_200_OK)
+  
+  def put(self, request, id):
+    data = request.data.copy()
+    # grab the activity instance
+    activity = get_object_or_404(Activity, id=id)
+    # pass new edited data into the serializer
+    serialized_activity = Activity_Serializer(activity, data=data, partial=True)
+    if serialized_activity.is_valid():
+      serialized_activity.save() # if new data is valid, save it to the database
+      return Response(status=s.HTTP_200_OK)
+    # if not, return errors
+    return Response(serialized_activity.errors, status=s.HTTP_400_BAD_REQUEST)
+  

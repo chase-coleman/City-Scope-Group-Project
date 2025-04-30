4bc7d4a4c0857fe551c929cfdd66816c56efc184
timskillbridge <timskillbridge@gmail.com>
2025-04-30 10:27:36 -0500

    login page in router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import HomePage from "./pages/HomePage";
+import Login from './pages/Login'
 
 const router = createBrowserRouter([
   {
@@ -12,8 +13,8 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: '/Login',
-        element: <LoginPage />,
+        path: '/login',
+        element: <Login />,
     },
     ],
   },

c4541015677aa0af89e4d44118b3018a301637a7
YOUR_NAME <YOUR_EMAIL>
2025-04-30 11:23:22 -0500

    got the .env google maps api key working.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import './index.css'
 import { Outlet } from 'react-router-dom';
@@ -7,8 +7,15 @@ import React from 'react';
 import { useNavigate, useHref } from 'react-router-dom';
 // https://beta.heroui.com/docs/guide/routing  ^^^
 
+
 const App = () => {
   const navigate = useNavigate()
+  const googleApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
+
+  useEffect(() => {
+    console.log(googleApiKey)
+  }, [])
 
   return (
     <>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [
     react(),
     tailwindcss()
   ],
-=======
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
->>>>>>> 53a75f1e616db67e823624b4cd81efe19626b198
-})
+});

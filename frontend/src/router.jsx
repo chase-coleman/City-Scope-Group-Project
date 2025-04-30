2e0894fc2c938a40150bc5240981038cb6f7c4b6
poptaro <noahahn199@gmail.com>
2025-04-30 14:24:18 -0500

    updated router.jsx slight update

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, useOutletContext } from "react-router-dom";
 import App from "./App";
 
 import HomePage from "./pages/HomePage";
-import LoginPage from './pages/Login'
+import LoginPage from "./pages/LoginPage";
 import TripViewPage from "./pages/TripViewPage";
 import ExplorePage from "./pages/ExplorePage";
 import TripsPage from "./pages/TripsPage";

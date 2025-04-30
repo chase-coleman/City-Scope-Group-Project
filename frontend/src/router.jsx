e074d12f608b6a6ec41ee15b6768e570ab49e4e4
YOUR_NAME <YOUR_EMAIL>
2025-04-30 11:49:52 -0500

    added a bit of code to Jack's Trips page so that we can use the navbar to navigate to it.

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import React from 'react';
 import Login from './pages/Login'
 import ExplorePage from "./pages/ExplorePage";
+import TripsPage from "./pages/TripsPage";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
     {
       path: '/explore',
       element: <ExplorePage />
+    },
+    {
+      path: '/trips',
+      element: <TripsPage />
     }
     ],
   },

6db3078deaf6e113dea794dc2d993bdd294dd1fa
YOUR_NAME <YOUR_EMAIL>
2025-04-30 14:43:13 -0500

    working on the explorePage

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { use, useEffect } from "react";
+import {createRoot} from 'react-dom/client';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import { useOutletContext } from "react-router-dom";
+import { userLogin } from "../Utilities/LoginPageUtils";
+const googleApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 const ExplorePage = () => {
 
+
   return (
     <>
-    <div className="explore-page-container  h-[calc(100vh-56px)] bg-red-500 flex">
-    <div className="left-side bg-blue-500 w-[20%]">
-    <h1>Filters</h1>
-    </div>
-    <div className="right-side bg-pink-500 w-[80%]">
-    <h1>Map Component</h1>
-    </div>
-    </div>
+      <div className="explore-page-container  h-[calc(100vh-56px)] bg-red-500 flex">
+        <div className="left-side bg-blue-500 w-[20%]">
+          <h1>Filters</h1>
+        </div>
+        <div className="right-side flex flex-col items-center bg-pink-500 w-[80%]">
+          <h1>Map Component</h1>
+          <div className="map-container bg-purple-200 w-[75%] h-[75%]">
+          <APIProvider apiKey={googleApiKey}>
+            <Map
+              defaultCenter={{ lat: 22.54992, lng: 0 }}
+              defaultZoom={3}
+              gestureHandling={"greedy"}
+              disableDefaultUI={true}
+            />
+          </APIProvider>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default ExplorePage
+export default ExplorePage;

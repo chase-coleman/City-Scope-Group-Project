6f56e83bd83222543d0eb6812e0147a132549900
YOUR_NAME <YOUR_EMAIL>
2025-04-30 14:06:43 -0500

    fixed user password hashing.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-=======
-import { heroui } from "@heroui/react";
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}" // Updated path to match package
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/line-clamp'),
+//   ],
+// };
 
->>>>>>> 53a75f1e616db67e823624b4cd81efe19626b198
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-<<<<<<< HEAD
-=======
-    "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}" // Updated path to match package
->>>>>>> 53a75f1e616db67e823624b4cd81efe19626b198
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-<<<<<<< HEAD
-    require('@tailwindcss/line-clamp'),
-  ],
-};
-=======
-    require('@tailwindcss/line-clamp'), 
-    heroui()
-  ],
-};
->>>>>>> 53a75f1e616db67e823624b4cd81efe19626b198
+

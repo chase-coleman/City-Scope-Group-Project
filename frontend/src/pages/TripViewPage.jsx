5d42d087173ec3b43eaf963f9bcddcfcacc2c348
poptaro <noahahn199@gmail.com>
2025-04-30 10:34:22 -0500

    another main pull, fixed login -> loginpage naming convention

--- frontend/src/pages/TripViewPage.jsx
+++ frontend/src/pages/TripViewPage.jsx
@@ -1,14 +1,20 @@
 
 export default function TripViewPage() {
   return (
-    <div className="flex flex-col ">
-      <div className="h-1/6">
+    <div className="flex flex-col h-dvh items-center justify-center p-4">
+      <div className="h-1/9">
         <p>
-          Trip Name
+          Trip Name: 
         </p>
         <p>
           Destination
         </p>
+      </div>
+      <div className="w-full h-4/9 border-2">
+        HI
+      </div>
+      <div className="w-full h-4/9 border-2">
+        HI 2
       </div>
     </div>
   )

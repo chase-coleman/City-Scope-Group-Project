db0fb587bdd542db5d14136160b8b5c13532d1b1
timskillbridge <timskillbridge@gmail.com>
2025-05-06 15:28:53 -0500

    Added API useage tracking and cuttoff before we're charged

--- backend/trip_advisor_api/models.py
+++ backend/trip_advisor_api/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class API_Usage(models.Model):
+class API(models.Model):
     api_key = models.CharField()
     key_owner = models.CharField()
-    key_Used = models.PositiveIntegerField(default=0)
+    key_used = models.PositiveIntegerField(default=0)
 
     def __rpr__(self):
-        return f"{self.key}, {self.key_used}"
+        return f"{self.api_key}, {self.key_used}"
     
     def str(self):
-        return f"{self.key}, {self.key_used}"
+        return f"{self.api_key}, {self.key_used}"
--- frontend/src/Utilities/TripAdvisorUtils.jsx
+++ frontend/src/Utilities/TripAdvisorUtils.jsx
@@ -1,6 +1,8 @@
 
 import axios from "axios";
-const TAKey = import.meta.env.TRIP_ADVISOR_KEY
+
+const TAKey = import.meta.env.VITE_TRIP_ADVISOR_KEY
+
 // --------------- API instance for all user functions ----------------
 
 export const trip_Advisor_Api = axios.create({
@@ -11,14 +13,36 @@ const headers = {
     Authorization: `key ${TAKey}`,
     'Content-Type': 'application/json',
   };
+  const grabKeyUse = async(TAKey) =>  {
+    const keyUse = await trip_Advisor_Api.get(`apiUsed/${TAKey}/`)
+    const key = keyUse.data.APIs[0].key_used
+    console.log(`You've used  ${key} calls`)
+  }
+  grabKeyUse(TAKey)
 
-  export const grabLocID = async (city, country, category, setLogError, setResults) => {
+
+  export const grabLocID = async (city, country, category, setLogError, setResults, latlong="", name="", address = "") => {
+    
+    const checkUsage = await trip_Advisor_Api.get(`apiUsed/${TAKey}/`)
+    const usedKey = checkUsage.data.APIs[0].key_used
+
+    if (usedKey >= 4990) {
+      alert(`API key is at ${usedKey} out of 5,000; get a new key`)
+      return
+    } else {
+      console.log(`API calls used for this key : ${usedKey}, adding 7 for the calls`)
+      await trip_Advisor_Api.put(`apiUsed/${TAKey}/`)
+      return
+    }
     try {
       const response = await trip_Advisor_Api.get('locID/', {
         params: {
           city: city,
           country: country,
           category: category,
+          ...(latlong ? { latlong }:{}), //spread operator with ternary statement for optional axios parameters.
+          ...(name ? {name}:{}),
+          ...(address? {address}:{})
         },
         headers: {
             Accept: 'application/json',

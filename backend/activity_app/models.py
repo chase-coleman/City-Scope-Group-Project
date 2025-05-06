661f328fd2b7487058ad771b3eebf7a888cd8d39
poptaro <noahahn199@gmail.com>
2025-05-06 14:10:10 -0500

    prior pull to fixing activities

--- backend/activity_app/models.py
+++ backend/activity_app/models.py
@@ -1,14 +1,14 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-from itinerary_app.models import Itinerary
+from trip_app.models import Trip
 
 class Activity(models.Model):
   name = models.CharField(max_length=50, null=False, blank=False)
   location = models.CharField(max_length=100, null=True, blank=True)
   address = models.CharField(max_length=100, null=True, blank=True)
   url = models.URLField(null=True, blank=True)
-  itineraries = models.ForeignKey(Itinerary, on_delete=models.CASCADE, related_name="activities", default=1)
+  trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="activity")
 
   def __str__(self):
     return f"Activity ID: {self.id}, Name: {self.name}, url: {self.url}"

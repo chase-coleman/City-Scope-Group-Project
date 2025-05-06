d01fc7e3b9c0adb4c7044f4b14d846efed58e5d8
poptaro <noahahn199@gmail.com>
2025-05-06 11:41:58 -0500

    Fixed models i think for stay being null, and only one stay per itinerary.

--- backend/itinerary_app/models.py
+++ backend/itinerary_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-# from trip_app.models import Trip
+from trip_app.models import Trip
+from stay_app.models import Stay
 
 class Itinerary(models.Model):
   date = models.DateField()
-  # trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="itinerary")
+  stay = models.ForeignKey(Stay, on_delete=models.SET_NULL, null=True, blank=True, related_name="stay", default=None)
+  trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="itinerary")
 
   def __str__(self):
     return f"{self.id}. Itinerary for date: {self.date}"

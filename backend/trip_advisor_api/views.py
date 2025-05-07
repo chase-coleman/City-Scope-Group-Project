8399d3607b336004556328384de29dee38c56be6
timskillbridge <timskillbridge@gmail.com>
2025-05-06 21:59:35 -0500

    expanded the trip advisor API search, initial query takes city with state or country and search term. Also takes number of results from 1-10, latlong, and address

--- backend/trip_advisor_api/views.py
+++ backend/trip_advisor_api/views.py
@@ -13,7 +13,8 @@ HTTP_204_NO_CONTENT,\
 HTTP_400_BAD_REQUEST,\
 HTTP_404_NOT_FOUND,\
 HTTP_500_INTERNAL_SERVER_ERROR,\
-HTTP_226_IM_USED
+HTTP_226_IM_USED,\
+HTTP_403_FORBIDDEN
 from .utility import get_locID
 from .models import API
 from .serializers import API_Used_Serializer
@@ -22,19 +23,42 @@ from .serializers import API_Used_Serializer
 # https://api.content.tripadvisor.com/api/v1/location/search?key=FCA3795BF57C4B3F8564DDA975F319D4&searchQuery=tokyo%2C%20japan&category=hotels&language=en
 class grabLocID(APIView):
     def get(self, request):
-        city = request.query_params.get("city")
-        country = request.query_params.get("country")
+        searchQuery = request.query_params.get("searchQuery")
         category = request.query_params.get("category")
-        name = request.query_params.get("name")
+        key = request.query_params.get("akey")
+        results = request.query_params.get("results")
         address = request.query_params.get("address")
         latlong = request.query_params.get("latlong")
+        
+        if not (searchQuery and key):
+            return Response({"error": "Missing query parameters: searchQuery, category, and key"}, status=HTTP_400_BAD_REQUEST)
+        parameters = {}
+        if address:
+            parameters['address'] = address
+        if latlong:
+            parameters['latlong'] = latlong
+        
+        loc_data = get_locID(searchQuery, category, key, results, address, latlong)
+        
 
-        if not (city and country and category):
-            return Response({"error": "Missing query parameters"}, status=HTTP_400_BAD_REQUEST)
-        locID = get_locID(city, country, category, name, address, latlong)
-        if locID:
-            return Response({"locinfo": locID}, status=HTTP_200_OK)
-        return Response({"error": "Failed to fetch data"}, status=HTTP_400_BAD_REQUEST)
+        # print(loc_data)
+        print(f"{searchQuery} {category} {key} fail getting loc_data")
+        if isinstance(loc_data, dict) and 'error' in loc_data:
+            return Response({
+                "error": loc_data['error']
+            }, status=HTTP_400_BAD_REQUEST
+            )
+        if loc_data:
+            print(f"{searchQuery} {category} {key} success")
+
+            return Response({
+                "locinfo":loc_data
+            }, status=HTTP_200_OK
+            )
+        return Response({
+            "error":"Failed to fetch location information"
+        }, status=HTTP_400_BAD_REQUEST
+        )
 
 
 class API_Usage(APIView):

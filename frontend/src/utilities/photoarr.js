bb01b288b74b2476e61270f01ca98d34180dbf94
timskillbridge <timskillbridge@gmail.com>
2025-05-11 20:38:31 -0500

    late push for the homepage stuff we worked on with working random image cube

---
+++ frontend/src/utilities/photoarr.js
@@ -0,0 +1,19 @@
+
+export const photoarr = [
+    "https://img.freepik.com/free-photo/old-bridge-chinese-park_1127-3746.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+    "https://img.freepik.com/premium-photo/seoul-city-skyline-best-view-south-korea_40171-97.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/aerial-view-tokyo-cityscape-with-fuji-mountain-japan_335224-148.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/premium-photo/las-vegas-united-states-march-19-2019-new-yorknew-york-complex-with-replica-statue-liberty_261932-17190.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/aerial-shot-downtown-los-angeles-night_181624-26585.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/premium-photo/view-skyscrapers-lit-up-night_1048944-30239468.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/beautiful-view-empire-states-skyscrapers-new-york-city_181624-6295.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/premium-photo/cairo-downtown-beautiful-view-nile-sailboats-egypt_400112-2094.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/premium-photo/aerial-view-downtown-cairo-nile-sunset-egypt_400112-2201.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/beautiful-majestic-castle_23-2150791539.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/vertical-shot-golden-gate-bridge-covered-fog_181624-11631.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/big-ben-westminster-bridge-sunset-london-uk_268835-1395.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://imgs.search.brave.com/OGo__bcwO1rhRhvcBNAftDmIkfCkftepayi3LRjNMts/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hczEu/ZnRjZG4ubmV0L3Yy/L2pwZy8wOS83Ny8w/Mi8xMC8xMDAwX0Zf/OTc3MDIxMDUyX0tW/M1dFOURxRTVhYWUz/TnBuOXEwTHdTZUZK/MnBlWkNNLmpwZw",
+"https://img.freepik.com/free-photo/landscape-hangzhou_1127-3720.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/free-photo/wat-arun-temple-twilight-bangkok-thailand_335224-772.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740",
+"https://img.freepik.com/premium-photo/saint-basil-s-cathedral-red-square-moscow-russia-night-winter_118086-2938.jpg?ga=GA1.1.391174315.1746803674&semt=ais_hybrid&w=740"
+]
